3cd14e43cfe62647dbeb2f4bf2aefda8017d8a60
Jordan Cooperman <jordancooperman@gmail.com>
2021-12-09 16:13:24 +0800

    Add router and split out shipment email generation into separate route

--- src/views/Home.js
+++ src/views/Home.js
@@ -1,130 +1,41 @@
-import React, { useState } from 'react'
-import styled from 'styled-components'
-import cloneDeep from 'lodash/cloneDeep'
+import React from 'react'
 import {
   Container,
-  Form,
-  Row,
-  Col
+  ListGroup
 } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
-import { setLocalData, getLocalData } from '../utils/storage'
-import { addressFactory } from '../factories'
-import useForm from '../hooks/useForm'
-import HeaderNav from '../components/Nav'
-import Address from '../components/Address'
-import Parcels from '../components/Parcels'
-import RateParcels from '../components/RateParcels'
-import Rates from '../components/Rates'
-import PurchasedRate from '../components/PurchasedRate'
-import NotionShipments from '../components/NotionShipments'
+import styled from 'styled-components'
 
 function Home () {
-  const [rateParcels, setRateParcels] = useState(getLocalData('rateParcels') || [])
-  const [rates, setRates] = useState(getLocalData('rates') || [])
-  const [purchasedRate, setPurchasedRate] = useState(getLocalData('purchasedRate') || null)
-  const {
-    input,
-    isLoading,
-    handleChange,
-    handleSubmit,
-    bulkUpdate
-  } = useForm({
-    resource: 'shipment',
-    action: 'create',
-    defaultInput: {
-      addressFrom: getLocalData('addressFrom') || addressFactory(),
-      addressTo: getLocalData('addressTo') || addressFactory(),
-      parcels: getLocalData('parcels') || []
-    },
-    massageInput: (input) => {
-      const massaged = cloneDeep(input)
-      massaged.parcels.forEach((parcel, i) => {
-        const qty = parcel.quantity
-        delete parcel.id
-        delete parcel.quantity
-
-        if (qty > 1) {
-          const copies = []
-          for (let j = 1; j < qty; j++) {
-            copies.push(Object.assign({}, parcel))
-          }
-          massaged.parcels.splice(i, 0, ...copies)
-        }
-      })
-      return massaged
-    },
-    afterChange: (newInput) => {
-      setLocalData('addressFrom', newInput.addressFrom)
-      setLocalData('addressTo', newInput.addressTo)
-      setLocalData('parcels', newInput.parcels)
-    },
-    afterSubmit: setRateData
-  })
-
-  function setRateData (data) {
-    setLocalData('rateParcels', data.parcels)
-    setLocalData('rates', data.rates)
-    setRateParcels(data.parcels)
-    setRates(data.rates)
-  }
-
-  function _setPurchasedRate (rate) {
-    setLocalData('purchasedRate', rate)
-    setPurchasedRate(rate)
-  }
-
   return (
-    <>
-      <HeaderNav
-        isLoading={isLoading}
-        setRateData={setRateData}
-        setPurchasedRate={_setPurchasedRate}
-        handleSubmit={handleSubmit}
-      />
+    <Bg className='bg-dark'>
+      <Content>
+        <ListGroup>
+          <ListGroup.Item>
+            <Link to='buy-postage'>Buy Postage</Link>
+          </ListGroup.Item>
 
-      <Container fluid>
-        <Row className='pt-5'>
-          <ColWithBg xs={12} sm={6} className='pb-5 pt-5'>
-            <NotionShipments bulkUpdate={bulkUpdate} />
-
-            <Form>
-              <Address
-                address={input.addressFrom}
-                name='addressFrom'
-                handleChange={handleChange}
-              />
-
-              <Address
-                address={input.addressTo}
-                name='addressTo'
-                handleChange={handleChange}
-              />
-
-              <Parcels
-                parcels={input.parcels}
-                handleChange={handleChange}
-              />
-            </Form>
-          </ColWithBg>
-
-          <Col xs={12} sm={6} className='pt-5'>
-            <PurchasedRate rate={purchasedRate} />
-            <Rates
-              rates={rates}
-              setPurchasedRate={_setPurchasedRate}
-            />
-            <RateParcels parcels={rateParcels} />
-          </Col>
-        </Row>
-      </Container>
-    </>
+          <ListGroup.Item>
+            <Link to='shipment-email'>Generate Shipment Email</Link>
+          </ListGroup.Item>
+        </ListGroup>
+      </Content>
+    </Bg>
   )
 }
 
-const ColWithBg = styled(Col)`
-  background-color: #eaeaea;
-  border-right: 1px solid #d5d5d5;
+const Bg = styled.div`
+  height: 100%;
+`
+
+const Content = styled(Container)`
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: center;
+  margin: 0 auto;
+  max-width: 500px;
 `
 
 export default Home
