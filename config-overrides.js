b93a443b1f07abdd72c078af2b3c29f95ed441bd
Jordan Cooperman <jordancooperman@gmail.com>
2024-03-24 13:15:53 -0700

    complete polyfills for webpack upgrade

--- config-overrides.js
+++ config-overrides.js
@@ -5,13 +5,19 @@ module.exports = function override(config) {
   Object.assign(fallback, {
     querystring: require.resolve("querystring-es3"),
     path: require.resolve("path-browserify"),
-    http: require.resolve("http-browserify"),
+    http: require.resolve("stream-http"),
     https: require.resolve("https-browserify"),
     stream: require.resolve("stream-browserify"),
-    util: require.resolve("util"),
-    url: require.resolve("url"),
-    buffer: require.resolve("buffer")
+    util: require.resolve("util/"),
+    url: require.resolve("url/"),
+    buffer: require.resolve("buffer/")
   });
   config.resolve.fallback = fallback;
+  config.plugins = (config.plugins || []).concat([
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+      Buffer: ["buffer", "Buffer"]
+    })
+  ]);
   return config;
 };
