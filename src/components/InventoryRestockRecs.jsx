fc9da9fdc7ce8657a4ff2ca0d62b9e40c04529ae
Jordan Cooperman <jordancooperman@gmail.com>
2025-04-05 07:23:05 -0700

    Get app working after login

--- src/components/InventoryRestockRecs.jsx
+++ src/components/InventoryRestockRecs.jsx
@@ -1,13 +1,17 @@
-import { CardColumns } from 'react-bootstrap'
+import { Row, Col } from 'react-bootstrap'
 
 import InventoryProductCard from './InventoryProductCard'
 
 
 function InventoryRestockRecs ({ products }) {
   return (
-    <CardColumns className='mb-4'>
-      {products.map((product, i) => (<InventoryProductCard key={i} product={product} />))}
-    </CardColumns>
+    <Row className='mb-4'>
+      {products.map((product, i) => (
+        <Col key={i} xs={12} sm={6} md={4} lg={3} className="mb-4">
+          <InventoryProductCard product={product} />
+        </Col>
+      ))}
+    </Row>
   )
 }
 
