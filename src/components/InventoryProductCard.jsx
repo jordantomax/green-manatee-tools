4e780a11e7b0fd8c868ebf69c63e2adca0bbc685
Jordan Cooperman <jordancooperman@gmail.com>
2025-04-08 09:51:04 -0700

    Continue styling inventory

--- src/components/InventoryProductCard.jsx
+++ src/components/InventoryProductCard.jsx
@@ -3,37 +3,14 @@ import {
   Card,
   Text,
   Button,
-  Group,
-  Stack,
   Badge,
-  Title
+  Title,
+  Table,
+  Box
 } from '@mantine/core'
 import { IconArrowRight } from '@tabler/icons-react'
 
 import api from '../utils/api'
-
-function RestockUnits ({ product }) {
-  const {
-    fba,
-    warehouse,
-    needFbaRestock,
-    needWarehouseRestock
-  } = product.restock
-  
-  return (
-    <>
-      <Group position="apart">
-        <Text size="sm">FBA restock:</Text>
-        <Badge color={needFbaRestock ? "green" : "gray"}>{fba}</Badge>
-      </Group>
-
-      <Group position="apart">
-        <Text size="sm">Warehouse restock:</Text>
-        <Badge color={needWarehouseRestock ? "green" : "gray"}>{warehouse}</Badge>
-      </Group>
-    </>
-  )
-}
 
 function Sales ({ sales }) {
   return sales.map((period, i) => {
@@ -68,47 +45,65 @@ function InventoryProductCard ({ product }) {
         </Button>
       </Card.Section>
 
-      <Card.Section p="md" withBorder>
-        <RestockUnits product={product} />
-      </Card.Section>
-
-      <Card.Section p="md" withBorder>
-        <Group>
-          <Text size="sm">Projected monthly sales:</Text>
-          <Text size="sm">{product.sales.amzProjectedMonthlyUnitSales}</Text>
-        </Group>
-        <Group>
-          <Text size="sm">30 day sales <IconArrowRight /> new:</Text>
-          <Text size="sm"><Sales sales={product.sales.amzUnitSalesBy30DayPeriods} /></Text>
-        </Group>
-        <Group>
-          <Text size="sm">Monthly growth rate:</Text>
-          <Text size="sm">{product.sales.amzWeightedMonthlyGrowthRate}</Text>
-        </Group>
-      </Card.Section>
-
-      <Card.Section p="md" withBorder>
-        <Group justify="space-between">
-          <Text size="sm">FBA stock:</Text>
-          <Text size="sm">{product.fba.stock}</Text>
-        </Group>
-        <Group justify="space-between">
-          <Text size="sm">FBA inbound:</Text>
-          <Text size="sm">{product.fba.inbound}</Text>
-        </Group>
-        <Group justify="space-between">
-          <Text size="sm">AWD stock:</Text>
-          <Text size="sm">{product.awd.stock || 0}</Text>
-        </Group>
-        <Group justify="space-between">
-          <Text size="sm">AWD inbound:</Text>
-          <Text size="sm">{product.awd.inbound || 0}</Text>
-        </Group>
-        <Group justify="space-between">
-          <Text size="sm">Warehouse stock:</Text>
-          <Text size="sm">{product.warehouse.stock}</Text>
-        </Group>
-      </Card.Section>
+      <Table withBorder>
+        <Table.Tbody>
+          {/* Restock */}
+          <Table.Tr>
+            <Table.Td><Text size="sm">FBA restock:</Text></Table.Td>
+            <Table.Td><Badge color={product.restock.needFbaRestock ? "green" : "gray"}>{product.restock.fba}</Badge></Table.Td>
+          </Table.Tr>
+          <Table.Tr>
+            <Table.Td><Text size="sm">Warehouse restock:</Text></Table.Td>
+            <Table.Td><Badge color={product.restock.needWarehouseRestock ? "green" : "gray"}>{product.restock.warehouse}</Badge></Table.Td>
+          </Table.Tr>
+          
+          {/* Sales */}
+          <Table.Tr>
+            <Table.Td colSpan={2}>
+              <Box fw={500} c="dimmed" py="xs">Sales</Box>
+            </Table.Td>
+          </Table.Tr>
+          <Table.Tr>
+            <Table.Td><Text size="sm">30 day sales:</Text></Table.Td>
+            <Table.Td><Text size="sm"><Sales sales={product.sales.amzUnitSalesBy30DayPeriods} /></Text></Table.Td>
+          </Table.Tr>
+          <Table.Tr>
+            <Table.Td><Text size="sm">Monthly change:</Text></Table.Td>
+            <Table.Td><Text size="sm">{product.sales.amzWeightedMonthlyGrowthRate}</Text></Table.Td>
+          </Table.Tr>
+          <Table.Tr>
+            <Table.Td><Text size="sm">Forecast sales:</Text></Table.Td>
+            <Table.Td><Text size="sm">{product.sales.amzProjectedMonthlyUnitSales}</Text></Table.Td>
+          </Table.Tr>
+          
+          {/* Inventory */}
+          <Table.Tr>
+            <Table.Td colSpan={2}>
+              <Box fw={500} c="dimmed" py="xs">Inventory</Box>
+            </Table.Td>
+          </Table.Tr>
+          <Table.Tr>
+            <Table.Td><Text size="sm">FBA stock:</Text></Table.Td>
+            <Table.Td><Text size="sm">{product.fba.stock}</Text></Table.Td>
+          </Table.Tr>
+          <Table.Tr>
+            <Table.Td><Text size="sm">FBA inbound:</Text></Table.Td>
+            <Table.Td><Text size="sm">{product.fba.inbound}</Text></Table.Td>
+          </Table.Tr>
+          <Table.Tr>
+            <Table.Td><Text size="sm">AWD stock:</Text></Table.Td>
+            <Table.Td><Text size="sm">{product.awd.stock || 0}</Text></Table.Td>
+          </Table.Tr>
+          <Table.Tr>
+            <Table.Td><Text size="sm">AWD inbound:</Text></Table.Td>
+            <Table.Td><Text size="sm">{product.awd.inbound || 0}</Text></Table.Td>
+          </Table.Tr>
+          <Table.Tr>
+            <Table.Td><Text size="sm">Warehouse stock:</Text></Table.Td>
+            <Table.Td><Text size="sm">{product.warehouse.stock}</Text></Table.Td>
+          </Table.Tr>
+        </Table.Tbody>
+      </Table>
     </Card>
   )
 }
