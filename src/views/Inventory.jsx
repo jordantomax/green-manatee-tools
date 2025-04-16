7e985d9ce58002e828a92c7c8c0f3ef2938adc35
Jordan Cooperman <jordancooperman@gmail.com>
2025-04-16 07:27:16 -0700

    Remove local error handling in inventory, that will be trigged globally now

--- src/views/Inventory.jsx
+++ src/views/Inventory.jsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import {
   Container,
   Button,
-  Alert,
   Badge,
   Title,
   Stack,
@@ -15,31 +14,26 @@ import InventoryRestockRecs from '../components/InventoryRestockRecs'
 
 function Inventory () {
   const [isLoading, setIsLoading] = useState(false)
-  const [errorMessage, setErrorMessage] = useState(null)
   const [data, setData] = useState(getLocalData('inventoryRecs'))
   const [datetime, setDatetime] = useState(new Date(getLocalData('inventoryRecsDatetime')))
 
   async function getRecommendations () {
-    if (errorMessage) setErrorMessage(null)
     setIsLoading(true)
-    const data = await api.getRecs()
-    setIsLoading(false)
-    setLocalData('inventoryRecs', data)
-    setData(data)
-    const date = new Date()
-    setLocalData('inventoryRecsDatetime', date)
-    setDatetime(date)
+    try {
+      const data = await api.getRecs()
+      setLocalData('inventoryRecs', data)
+      setData(data)
+      const date = new Date()
+      setLocalData('inventoryRecsDatetime', date)
+      setDatetime(date)
+    } finally {
+      setIsLoading(false)
+    }
   }
 
   return (
     <Container size="md" py="xl">
       <Stack spacing="xl">
-        {errorMessage && (
-          <Alert color="red" title="Error">
-            {errorMessage}
-          </Alert>
-        )}
-
         <Title order={2}>Inventory Manager</Title>
 
         <Group>
