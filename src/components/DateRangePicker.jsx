3d3ce3f195a7b6250f209e1b9d66de8db4d7bbef
Jordan Cooperman <jordancooperman@gmail.com>
2025-09-01 20:32:47 -0700

    Add some minor styling to the new date range picker

--- src/components/DateRangePicker.jsx
+++ src/components/DateRangePicker.jsx
@@ -1,6 +1,8 @@
-import { Group, Popover } from "@mantine/core"
+import { Group, Popover, Paper } from "@mantine/core"
 import { DatePicker, DateInput } from "@mantine/dates"
+import { IconChevronRight } from '@tabler/icons-react'
 import { dateRangePresets } from '@/utils/date'
+
 
 function DateRangePicker({ 
   value, 
@@ -9,22 +11,40 @@ function DateRangePicker({
   return (
     <Popover position="bottom-start">
       <Popover.Target>
-        <Group gap="xs" align="flex-end">
-          <DateInput
-            placeholder="Start date"
-            value={value.startDate}
-            onChange={(date) => onChange({ ...value, startDate: date })}
-            popoverProps={{ disabled: true }}
-            style={{ minWidth: 120 }}
-          />
-          <DateInput
-            placeholder="End date"
-            value={value.endDate}
-            onChange={(date) => onChange({ ...value, endDate: date })}
-            popoverProps={{ disabled: true }}
-            style={{ minWidth: 120 }}
-          />
-        </Group>
+        <Paper p="0">
+          <Group 
+            gap="0" 
+            style={{ 
+              flexWrap: 'nowrap', 
+              overflow: 'hidden', 
+              width: 250
+            }}
+          >
+            <DateInput
+              size="sm"
+              variant="unstyled"
+              placeholder="Start Date"
+              value={value.startDate}
+              onChange={(date) => onChange({ ...value, startDate: date })}
+              popoverProps={{ disabled: true }}
+              px="xs"
+              valueFormat="M/D/YYYY"
+              styles={{ input: { fieldSizing: 'content' } }}
+            />
+            <IconChevronRight size={16} />
+            <DateInput
+              size="sm"
+              variant="unstyled"
+              placeholder="End Date"
+              value={value.endDate}
+              onChange={(date) => onChange({ ...value, endDate: date })}
+              popoverProps={{ disabled: true }}
+              px="xs"
+              valueFormat="M/D/YYYY"
+              styles={{ input: { fieldSizing: 'content' } }}
+            />
+          </Group>
+        </Paper>
       </Popover.Target>
       <Popover.Dropdown>
         <DatePicker 
